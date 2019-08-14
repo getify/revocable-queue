9490a4356126d2ffc6dbcd55a7673627e7ac8350
Kyle Simpson <getify@gmail.com>
2019-08-13 21:13:09 -0500

    adding 'eventState(..)' closes #1, improve test suite closes #2

--- index.js
+++ index.js
@@ -1,116 +1,217 @@
-"use strict";
+(function UMD(name,context,definition){
+	/* istanbul ignore next */if (typeof define === "function" && define.amd) { define(definition); }
+	/* istanbul ignore next */else if (typeof module !== "undefined" && module.exports) { module.exports = definition(); }
+	/* istanbul ignore next */else { context[name] = definition(); }
+})("RevocableQueue",this,function DEF(){
 
-var API = module.exports = {
-	create,
-	lazyZip,
-};
-Object.defineProperty(module.exports,"EMPTY",{ value: {}, writable: false, configurable: false, enumerable: false, });
+	// a basic event-emitter, if you don't already have one
+	class EventEmitter {
+		constructor() {
+			this.listeners = {};
+		}
+		emit(evtName,...args) {
+			for (let handler of (this.listeners[evtName] || [])) {
+				try { handler.apply(this,args); }
+				catch (err) {}
+			}
+			return this;
+		}
+		on(evtName,handler) {
+			this.listeners[evtName] = this.listeners[evtName] || new Set();
+			this.listeners[evtName].add(handler);
+			return this;
+		}
+		once(evtName,handler) {
+			var onEvt = (...args) => {
+				this.off(evtName,handler);
+				handler.apply(this,args);
+			};
+			return this.on(evtName,onEvt);
+		}
+		removeListener(evtName,handler) {
+			if (this.listeners[evtName]) {
+				this.listeners[evtName].delete(handler);
+			}
+			return this;
+		}
+		off(...args) {
+			return this.removeListener(...args);
+		}
+	}
 
 
-// ******************************
-
-function create() {
-	var queue = [];
-	var signals = [];
-
-	var publicAPI = {
-		add,
-		insertFirst,
-		next,
+	var moduleAPI = {
+		create,
+		lazyZip,
+		eventState,
+		EventEmitter,
 	};
+	Object.defineProperty(moduleAPI,"EMPTY",{ value: {}, writable: false, configurable: false, enumerable: false, });
 
-	return publicAPI;
+	return moduleAPI;
 
 
 	// ******************************
 
-	function next() {
-		return new Promise(function c(res){
-			signals.push(res);
-			notify();
-		});
-	}
+	function create() {
+		var queue = [];
+		var signals = [];
 
-	function add(v) {
-		var entry = {
-			use(take = true) {
+		var queueAPI = {
+			add,
+			insertFirst,
+			next,
+		};
+
+		return queueAPI;
+
+
+		// ******************************
+
+		function next() {
+			return new Promise(function c(res){
+				signals.push(res);
+				notify();
+			});
+		}
+
+		function add(v) {
+			var entry = {
+				use(take = true) {
+					// entry still pending?
+					if (entry.use) {
+						if (take) {
+							entry.use = null;
+						}
+						return v;
+					}
+					return moduleAPI.EMPTY;
+				},
+			};
+			queue.push(entry);
+
+			// microtask defer
+			Promise.resolve().then(notify);
+
+			return entry.use;
+		}
+
+		function insertFirst(v) {
+			var use = add(v);
+			// move entry to the front of the line
+			if (queue.length > 1) {
+				queue.unshift(queue.pop());
+			}
+			return use;
+		}
+
+		function notify() {
+			while (queue.length > 0 && signals.length > 0) {
+				let entry = queue.shift();
 				// entry still pending?
 				if (entry.use) {
-					if (take) {
-						entry.use = null;
-					}
-					return v;
+					signals.shift()(entry.use);
 				}
-				return API.EMPTY;
-			},
-		};
-		queue.push(entry);
-
-		// microtask defer
-		Promise.resolve().then(notify);
-
-		return entry.use;
-	}
-
-	function insertFirst(v) {
-		var use = add(v);
-		// move entry to the front of the line
-		if (queue.length > 1) {
-			queue.unshift(queue.pop());
-		}
-		return use;
-	}
-
-	function notify() {
-		while (queue.length > 0 && signals.length > 0) {
-			let entry = queue.shift();
-			// entry still pending?
-			if (entry.use) {
-				signals.shift()(entry.use);
 			}
 		}
 	}
-}
 
-async function *lazyZip(...queues) {
-	var accessors = [];
+	async function *lazyZip(...queues) {
+		var accessors = [];
 
-	while (true) {
-		// wait on either previously un-used accessors or new
-		// accessors from the queues
-		let waiters = [];
-		for (let [idx,queue,] of queues.entries()) {
-			waiters[idx] = accessors[idx] || queue.next();
-		}
-
-		// wait for all accessors to resolve
-		accessors = await Promise.all(waiters);
-
-		// peek at accessor results
-		let results = [];
-		for (let [idx,accessor,] of accessors.entries()) {
-			results[idx] = accessor(/*take=*/false);
-		}
-
-		// all results available?
-		if (!results.includes(API.EMPTY)) {
-			// mark all accessors as used
-			for (let accessor of accessors) {
-				accessor(/*take=*/true);
+		while (true) {
+			// wait on either previously un-used accessors or new
+			// accessors from the queues
+			let waiters = [];
+			for (let [idx,queue,] of queues.entries()) {
+				waiters[idx] = accessors[idx] || queue.next();
 			}
 
-			// send this set of results
-			yield results;
+			// wait for all accessors to resolve
+			accessors = await Promise.all(waiters);
 
-			// force discarding of accessors
-			results.fill(API.EMPTY);
-		}
+			// peek at accessor results
+			let results = [];
+			for (let [idx,accessor,] of accessors.entries()) {
+				results[idx] = accessor(/*take=*/false);
+			}
 
-		// discard any used accessors (for next iteration)
-		for (let [idx,val,] of results.entries()) {
-			if (val === API.EMPTY) {
-				accessors[idx] = null;
+			// all results available?
+			if (!results.includes(moduleAPI.EMPTY)) {
+				// mark all accessors as used
+				for (let accessor of accessors) {
+					accessor(/*take=*/true);
+				}
+
+				// send this set of results
+				yield results;
+
+				// force discarding of accessors
+				results.fill(moduleAPI.EMPTY);
+			}
+
+			// discard any used accessors (for next iteration)
+			for (let [idx,val,] of results.entries()) {
+				if (val === moduleAPI.EMPTY) {
+					accessors[idx] = null;
+				}
 			}
 		}
 	}
-}
+
+	async function eventState(segments) {
+		// create a revocable queue to represent each gate segment
+		var queues = segments.map(function createQueue(segment){
+			var q = create();
+			var revoke;
+
+			q.segment = segment;
+			q.wait = wait;
+			q.signal = signal;
+
+			if (segment.onEvent) {
+				segment.listener.on(segment.onEvent,signal);
+			}
+			if (segment.offEvent) {
+				segment.listener.on(segment.offEvent,wait);
+			}
+			if (segment.status) {
+				signal();
+			}
+
+			return q;
+
+
+			// **********************
+
+			function wait() {
+				if (revoke) {
+					revoke();
+					revoke = null;
+				}
+			}
+
+			function signal() {
+				if (!revoke) {
+					revoke = q.add(true);
+				}
+			}
+		});
+
+		// wait for all events to be fired & active at the same time
+		await lazyZip(...queues).next();
+
+		// unsubscribe any listeners, avoid memory leaks
+		queues.forEach(function unsubscribe(q){
+			if (q.segment.onEvent) {
+				q.segment.listener.off(q.segment.onEvent,q.signal);
+			}
+			if (q.segment.offEvent) {
+				q.segment.listener.off(q.segment.offEvent,q.wait);
+			}
+			q.wait = q.signal = q.segment = null;
+		});
+		segments.length = queues.length = 0;
+	}
+
+});
--- test.js
+++ test.js
@@ -3,15 +3,29 @@
 var util = require("util");
 
 var delay = util.promisify(setTimeout);
-var RevocableQueue = require("./index.js");
+
+// NOTE: Right now, this is a very incomplete test suite. It
+// covers basic core of queue add / revoke, and a bit of
+// the eventState (and thus lazyZip). These tests should
+// eventually be made a lot more comprehensive.
+(async function runAllTests(){
+	try {
+		await test1();
+		await test2();
+		console.log("all tests passed.");
+		process.exitCode = 0;
+	}
+	catch (errMsg) {
+		console.error(errMsg);
+		process.exitCode = 1;
+	}
+})();
 
 
-// NOTE: Right now, this is a very incomplete test suite,
-// only covering the core queue read & revoke. Many more
-// edge cases should eventually be tested, as well as the
-// lazyZip() function.
-(async function test(){
-	var expected = [1,2,4,5,6,8,10];
+// ***********************************
+
+async function test1() {
+	var expected = [1,2,4,5,6,8,10,];
 	var actual = [];
 
 	var q = RevocableQueue.create();
@@ -49,14 +63,73 @@ var RevocableQueue = require("./index.js");
 		await delay(30);
 	}
 
-	await readComplete;
+	await Promise.race([
+		readComplete,
+		timeout(1000),
+	]);
 
-	if (JSON.stringify(expected) === JSON.stringify(actual)) {
-		console.log("tests passed.");
-		process.exitCode = 0;
+	if (JSON.stringify(expected) !== JSON.stringify(actual)) {
+		throw new Error(`read/revoke tests failed.\n  expected: ${expected}\n  actual: ${actual}`);
 	}
-	else {
-		console.error("tests failed.");
-		process.exitCode = 1;
+}
+
+async function test2() {
+	var expected = ["one","two","three","four","five","six",];
+	var actual = [];
+
+	var emitters = [
+		new RevocableQueue.EventEmitter(),
+		new RevocableQueue.EventEmitter(),
+		new RevocableQueue.EventEmitter(),
+	];
+
+	var gate = RevocableQueue.eventState([
+		{ listener: emitters[0], onEvent: "yes", offEvent: "no", status: false, },
+		{ listener: emitters[1], onEvent: "yes", offEvent: "no", status: true, },
+		{ listener: emitters[2], onEvent: "yes", offEvent: "no", status: false, },
+	]);
+
+	(async function emitting(){
+		await delay(50);
+		actual.push("one");
+		emitters[0].emit("yes");
+
+		await delay(50);
+		actual.push("two");
+		emitters[1].emit("no");
+
+		await delay(50);
+		actual.push("three");
+		emitters[2].emit("yes");
+
+		await delay(50);
+		actual.push("four");
+		emitters[0].emit("no");
+		emitters[1].emit("yes");
+
+		await delay(50);
+		actual.push("five");
+		emitters[1].emit("yes");
+
+		await delay(50);
+		actual.push("six");
+		emitters[0].emit("yes");
+
+		await delay(50);
+		actual.push("nope");
+	})();
+
+	await Promise.race([
+		gate,
+		timeout(1000),
+	]);
+
+	if (JSON.stringify(expected) !== JSON.stringify(actual)) {
+		throw new Error(`eventState tests failed.\n  expected: ${expected}\n  actual: ${actual}`);
 	}
-})().catch(console.error);
+}
+
+async function timeout(ms) {
+	await delay(ms);
+	throw new Error("Timeout!");
+}
